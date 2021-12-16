ce6fbcd2dba3b415734144a7bb627eb9ecdc9109
rasyidarazak <rasyidarazaks@gmail.com>
2021-12-16 17:39:08 +0700

    fix inversion value

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Brute Force Algorithm
 function getInvCount(arr){
     let inv_count = 0;
+    $('#invertion-value').html("");
     for(let i=0; i<arr.length-1; i++){
         for(let j=i+1; j<arr.length; j++){
-            if(arr[i] > arr[j]) 
+            if(arr[i] > arr[j]) {
                 $('#invertion-value').append(" (" + arr[i] + "," +arr[j] + ") ");
                 inv_count++;
+            }  
         }
     }
     return inv_count;
