8df51b81c8f2e6d89f0abb48931eb11e094036ed
rasyidarazak <rasyidarazaks@gmail.com>
2021-12-16 17:12:09 +0700

    update function invertion with jquery

--- js/script.js
+++ js/script.js
@@ -1,13 +1,37 @@
-arr = [1, 20, 6, 4, 5];
- 
+// Brute Force Algorithm
 function getInvCount(arr){
     let inv_count = 0;
     for(let i=0; i<arr.length-1; i++){
         for(let j=i+1; j<arr.length; j++){
-            if(arr[i] > arr[j]) inv_count++;
+            if(arr[i] > arr[j]) 
+                $('#invertion-value').append(" (" + arr[i] + "," +arr[j] + ") ");
+                inv_count++;
         }
     }
     return inv_count;
 }
 
-// document.write("Number of inversions are "+ getInvCount(arr));
+// Init array
+arr = [];
+
+$("#addArr").on("click", function () {
+    // Add value to array
+    let val = document.getElementById("val").value;
+    if (!isNaN(val)){
+        arr.push(val);
+    }
+
+    // Show array in html
+    $("#arr").html("");
+    $('#arr').append("{ " + arr + " }");
+
+    // Show button if array not empty
+    if (arr.length > 0){
+        $("#invertion-btn").html("<button type='button' class='btn btn-sm btn-dark' id='invertion-calculate'>Calculate Invertion</button>");
+    }
+
+    $("#invertion-calculate").on("click", function () {
+        $("#invertion-result").html("Invertion Result = " + getInvCount(arr));
+    });
+});
+
